329b792f96e504638c64b69e62309f38f1d135b5
RxTavares <rafatcb0133@gmail.com>
2026-04-13 22:35:33 -0300

    Atualização

--- src/main/java/dobackaofront/Ponto_flutuante.java
+++ src/main/java/dobackaofront/Ponto_flutuante.java
@@ -5,16 +5,29 @@ public class Ponto_flutuante {
 
         // soma, subtração,divisão, resto
         // cada variavel tem um tipo de dado
-        //decimais - ponto flutuante,float
+        //inteiros - byte,short,int,long
+        //decimais - ponto flutuante,float,double
+        //logicos - boolean (true,false)
+        //char - caracteres - (aspas simples)
 
-        float x = 2.2f;
-        float y = 1.0f;
+        byte inteiro_pequeno = 127;
+        short inteiro_maior_que_byte =32767 ;
+        int inteiro_normal =2121561516 ;
+        long inteiro_grande = 21215615162121561l;
 
-        float resultado = x + y;
-        System.out.println(resultado);
+        //decimal
+        float decimal_pequeno = 132.31232323123123123123123123123123123123123123123123123123f;
+        double decimal_maior = 199.1232131231234214123221324141232324214123232421323213412323;
 
+        //logico - boolean
+        boolean valor_logico1 = true;
+        boolean valor_logico2 = false;
 
+        //caracter char
+        char caracter = 'a';
+        String texto = "Rafael Tavares";
 
+        System.out.println(valor_logico1);
 
     }
 }
