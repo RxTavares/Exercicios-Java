0f545ebe192017f76f9547f08cbcb66e22b372b8
RxTavares <rafatcb0133@gmail.com>
2026-04-13 21:57:22 -0300

    Initial commit

---
+++ src/main/java/dobackaofront/Calculadora.java
@@ -0,0 +1,27 @@
+package dobackaofront;
+
+public class Calculadora {
+      static void main(String[] args) {
+    // soma, subtração,divisão, resto
+    // cada variavel tem um tipo de dado]
+    int x = 10;
+    int y = 2;
+    int soma,subtracao,multiplicacao,divisao,resto;
+
+    //calculadora
+          soma = x + y;
+          System.out.println("O resultado de "+x+" + "+y+" = "+soma);
+
+          subtracao = x - y;
+          System.out.println("O resultado de "+x+" - "+y+" = "+subtracao);
+
+          multiplicacao = x * y;
+          System.out.println("O resultado de "+x+" * "+y+" = "+multiplicacao);
+
+          divisao = x / y;
+          System.out.println("O resultado de "+x+" / "+y+" = "+divisao);
+
+          resto = x % y;
+          System.out.println("O resultado de "+x+" % "+y+" = "+resto);
+    }
+}
---
+++ src/main/java/dobackaofront/Ponto_flutuante.java
@@ -0,0 +1,20 @@
+package dobackaofront;
+
+public class Ponto_flutuante {
+    static void main(String[] args){
+
+        // soma, subtração,divisão, resto
+        // cada variavel tem um tipo de dado
+        //decimais - ponto flutuante,float
+
+        float x = 2.2f;
+        float y = 1.0f;
+
+        float resultado = x + y;
+        System.out.println(resultado);
+
+
+
+
+    }
+}
