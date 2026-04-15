15c08f45d5a62c974f43d24a8d710a9697e05558
RxTavares <rafatcb0133@gmail.com>
2026-04-15 20:49:20 -0300

    Atualização

--- src/main/java/dobackaofront/Manipulação_string.java
+++ src/main/java/dobackaofront/Manipulação_string.java
@@ -4,9 +4,9 @@ package dobackaofront;
         static void main(String[] args){
         String nome ="Do Back ao front - Progração Full Stack";
 
-        String primeiro_nome:
-        String ultimo_nome
-        String texto_substituto
+        String primeiro_nome;
+        String ultimo_nome;
+        String texto_substituto;
 
             System.out.println(nome);
 
