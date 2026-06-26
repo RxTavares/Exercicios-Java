e13708fff33ae7ddc3c34f0becad148c38e19195
RxTavares <rafatcb0133@gmail.com>
2026-06-25 23:01:34 -0300

    Ptojetos e exercicios

---
+++ src/main/java/Exercicios_Apostila/Notas.java
@@ -0,0 +1,28 @@
+package Exercicios_Apostila;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Notas {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US );
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite a primeira nota: ");
+        double nota1 = sc.nextDouble();
+
+        System.out.print("Digite a segunda nota: ");
+        double nota2 = sc.nextDouble();
+
+        double Nota_final = nota1 + nota2;
+
+        if (Nota_final > 60.00) {
+            System.out.println("Nota final " + String.format("%.2f",Nota_final));
+            System.out.println("Aprovado");
+        } else {
+            System.out.println("Reprovado");
+        }
+    }
+}
